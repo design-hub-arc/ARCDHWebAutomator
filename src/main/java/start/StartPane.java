129d818ab1f4c6442ef3e9bb590cab8694ade715
IronHeart7334 <greengrappler12@gmail.com>
2019-11-08 13:36:55 -0800

    GridBagLayout is annoying. Would BoxLayout allow me to vertically align components?

--- src/main/java/start/StartPane.java
+++ src/main/java/start/StartPane.java
@@ -2,18 +2,20 @@ package start;
 
 import io.FileSelector;
 import io.QueryFileReader;
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import pages.AccountBalancePage;
 import pages.GoogleSearch;
 
@@ -33,6 +35,9 @@ public class StartPane extends JPanel{
         
         
         //browser selection
+        JPanel browserPanel = new JPanel();
+        browserPanel.setLayout(new BorderLayout());
+        browserPanel.add(new JLabel("Select Browser to use"), BorderLayout.PAGE_START);
         ButtonGroup selectedBrowser = new ButtonGroup();
         
         //todo: check which browsers the user has installed
@@ -47,14 +52,21 @@ public class StartPane extends JPanel{
         browserSelection.add(chrome);
         browserSelection.add(fireFox);
         
+        browserPanel.add(browserSelection, BorderLayout.CENTER);
+        
         gbc.fill = GridBagConstraints.BOTH;
         gbc.weightx = 1;
         gbc.weighty = 1;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        add(browserSelection, gbc);
+        add(browserPanel, gbc);
+        
         
         //select which automation to run
+        JPanel autoPanel = new JPanel();
+        autoPanel.setLayout(new BorderLayout());
+        autoPanel.add(new JLabel("Select which automation to run"), BorderLayout.PAGE_START);
+        
         ButtonGroup automations = new ButtonGroup();
         JRadioButton gs = new JRadioButton("Google searches");
         JRadioButton actBal = new JRadioButton("Account Balances");
@@ -64,15 +76,21 @@ public class StartPane extends JPanel{
         JPanel autoSel = new JPanel();
         autoSel.add(gs);
         autoSel.add(actBal);
+        autoPanel.add(autoSel, BorderLayout.CENTER);
         
         gbc.gridy = 1;
-        add(autoSel, gbc);
+        add(autoPanel, gbc);
+        
+        //select web driver path
+        gbc.gridy = 2;
+        add(webDriverPanel(), gbc);
+        
         
         JButton fileChooser = new JButton("Choose data source");
         fileChooser.addActionListener((e)->{
             chooseFile();
         });
-        gbc.gridy = 2;
+        gbc.gridy = 3;
         add(fileChooser, gbc);
         
         JButton run = new JButton("Run automation");
@@ -94,8 +112,34 @@ public class StartPane extends JPanel{
                 ex.printStackTrace();
             }
         });
-        gbc.gridy = 3;
+        gbc.gridy = 4;
         add(run, gbc);
+    }
+    
+    private final JPanel webDriverPanel(){
+        JPanel webDriverPanel = new JPanel();
+        webDriverPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        
+        webDriverPanel.add(new JLabel("Select your web driver"), gbc);
+        
+        JTextArea currentDriver = new JTextArea("no driver selected");
+        currentDriver.setEditable(false);
+        gbc.gridy = 1;
+        webDriverPanel.add(currentDriver, gbc);
+        
+        JButton choosePath = new JButton("Choose web driver");
+        choosePath.addActionListener((e)->{
+            findWebDriver();
+            currentDriver.setText(webDriverPath);
+        });
+        gbc.gridy = 2;
+        webDriverPanel.add(choosePath, gbc);
+        
+        return webDriverPanel;
     }
     
     private void findWebDriver(){
