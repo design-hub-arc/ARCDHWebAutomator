5ecf15ba03f1389e823998d2a592351b1095b8cf
IronHeart7334 <greengrappler12@gmail.com>
2020-02-18 12:19:23 -0800

    I may wind up doing gradle sub-projects for this, but that might be a bit excessive

--- src/main/java/launcher/Launcher.java
+++ src/main/java/launcher/Launcher.java
@@ -1,5 +1,7 @@
 package launcher;
 
+import logging.ApplicationLog;
+
 /**
  * The Launcher is analogous to the Application class,
  * handling updating and starting the application.\
@@ -7,6 +9,24 @@ package launcher;
  * @author Matt Crow
  */
 public class Launcher {
+    private final ApplicationLog log;
+    
+    private static Launcher instance;
+    
+    private Launcher(){
+        if(instance != null){
+            throw new RuntimeException("Cannot instantiate multiple launchers, use getInstance() instead.");
+        }
+        log = new ApplicationLog();
+    }
+    
+    public Launcher getInstance(){
+        if(instance == null){
+            instance = new Launcher();
+        }
+        return instance;
+    }
+    
     public void launch(){
         LauncherFrame window = new LauncherFrame();
     }
