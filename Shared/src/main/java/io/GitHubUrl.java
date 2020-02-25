a3b44e5d53fbe984cfa888b13cbb71ec93b0f14e
IronHeart7334 <greengrappler12@gmail.com>
2020-02-25 12:54:46 -0800

    need to work on githuburl

---
+++ Shared/src/main/java/io/GitHubUrl.java
@@ -0,0 +1,13 @@
+package io;
+
+/**
+ *
+ * @author Matt
+ */
+public class GitHubUrl {
+    private final String repository;
+    
+    public GitHubUrl(String repositoryName){
+        repository = repositoryName;
+    }
+}
